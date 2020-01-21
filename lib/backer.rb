31f4ae3385b4d8eada7dbb205329120f67b751dd
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-01-21 17:27:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,11 +2,15 @@ class Backer
   
   def initialize(name)
     @name = name
-    @backed_projects = []
+    backed_projects = []
   end 
   
   def name 
     @name
+  end 
+  
+  def backed_projects
+    @backed_projects
   end 
 
 
